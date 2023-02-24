d146966f978c327aa86bb6e6d44c64d202685953
Shantanu Methikar <smethikar18@gmail.com>
2023-02-24 10:45:31 +0530

    Add WebView2 to render local index.html

--- WinUi3AndJsInterop/MainWindow.xaml.cs
+++ WinUi3AndJsInterop/MainWindow.xaml.cs
@@ -1,23 +1,9 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Xaml;
+using Microsoft.Web.WebView2.Core;
 
 namespace WinUi3AndJsInterop
 {
@@ -28,12 +14,19 @@ namespace WinUi3AndJsInterop
     {
         public MainWindow()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            webView2.Loaded += OnWebView2Loaded;
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void OnWebView2Loaded(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            await webView2.EnsureCoreWebView2Async();
+            webView2.CoreWebView2.SetVirtualHostNameToFolderMapping(hostName: "shan.tutorials.winui3",
+                folderPath: "Assets",
+                accessKind: CoreWebView2HostResourceAccessKind.Allow);
+
+            webView2.Source = new Uri("https://shan.tutorials.winui3/html/index.html");
+
         }
     }
 }
