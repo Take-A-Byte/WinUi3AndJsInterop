a0ec0c659c913b9d4b2c291eb35d102677454fe1
Shantanu Methikar <smethikar18@gmail.com>
2023-03-25 14:52:38 +0530

    Add native code - "Add" function - to be accessed from JS

---
+++ NativeCode/PlayWithNumbers.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+#include "PlayWithNumbers.h"
+#include "PlayWithNumbers.g.cpp"
+
+namespace winrt::NativeCode::implementation
+{
+    int32_t PlayWithNumbers::Add(int32_t num1, int32_t num2)
+    {
+        return num1 + num2;
+    }
+}
