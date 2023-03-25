fb09dae0fb2135c6040434fb9cde8236b8107e8b
Shantanu Methikar <smethikar18@gmail.com>
2023-03-25 15:37:56 +0530

    Inject NativeCode object and call Add function from JS and some styling changes

--- WinUi3AndJsInterop/MainWindow.xaml.cs
+++ WinUi3AndJsInterop/MainWindow.xaml.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 
 using System;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.Web.WebView2.Core;
+using Windows.Graphics;
+using Microsoft.UI.Windowing;
 
 namespace WinUi3AndJsInterop
 {
@@ -15,6 +18,20 @@ namespace WinUi3AndJsInterop
         public MainWindow()
         {
             InitializeComponent();
+            // Retrieve the window handle (HWND) of the current (XAML) WinUI 3 window.
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+
+            // Retrieve the WindowId that corresponds to hWnd.
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+
+            // Lastly, retrieve the AppWindow for the current (XAML) WinUI 3 window.
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+
+            if (appWindow != null)
+            {
+                appWindow.Resize(new SizeInt32(800, 300));
+            }
+
             webView2.Loaded += OnWebView2Loaded;
         }
 
@@ -23,7 +40,10 @@ namespace WinUi3AndJsInterop
             await webView2.EnsureCoreWebView2Async();
             webView2.CoreWebView2.SetVirtualHostNameToFolderMapping(hostName: "shan.tutorials.winui3",
                 folderPath: "Assets",
-                accessKind: CoreWebView2HostResourceAccessKind.Allow);
+                accessKind: CoreWebView2HostResourceAccessKind.Allow); 
+            var dispatchAdapter = new NativeAdapter.DispatchAdapter();
+            webView2.CoreWebView2.AddHostObjectToScript(
+                "NativeCode", dispatchAdapter.WrapNamedObject("NativeCode", dispatchAdapter));
 
             webView2.Source = new Uri("https://shan.tutorials.winui3/html/index.html");
 
